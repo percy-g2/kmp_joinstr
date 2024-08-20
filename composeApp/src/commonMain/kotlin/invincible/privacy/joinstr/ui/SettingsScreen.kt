802f9d393b302911d70b7fdb4291229a114be246
percy-g2 <gahlotpercy@gmail.com>
2024-08-21 01:18:53 +0530

    SettingsScreen more build out.
    Signed-off-by: percy-g2 <gahlotpercy@gmail.com>

--- composeApp/src/commonMain/kotlin/invincible/privacy/joinstr/ui/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/invincible/privacy/joinstr/ui/SettingsScreen.kt
@@ -1,6 +1,7 @@
 package invincible.privacy.joinstr.ui
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,16 +13,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,6 +51,10 @@ import kotlinx.coroutines.launch
 fun SettingsScreen(
     onBackPress: () -> Unit
 ) {
+    var nodeUrl by remember { mutableStateOf("http://127.0.0.1") }
+    var username by remember { mutableStateOf("user") }
+    var password by remember { mutableStateOf("pass") }
+    var port by remember { mutableStateOf("38332") }
     val coroutineScope = rememberCoroutineScope()
     val settings: Flow<Settings?> = store.updates
     val selectedTheme by settings.collectAsState(initial = Settings(selectedTheme = 0))
@@ -47,15 +62,16 @@ fun SettingsScreen(
     val themesList = Theme.entries.map { theme ->
         ThemeData(theme.title, theme.description)
     }
-    Column {
 
-        Spacer(modifier = Modifier.height(2.dp))
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        Spacer(modifier = Modifier.height(8.dp))
 
         Row(
             horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically
         ) {
-
             Spacer(modifier = Modifier.width(8.dp))
 
             IconButton(
@@ -68,7 +84,7 @@ fun SettingsScreen(
             Spacer(modifier = Modifier.width(2.dp))
 
             Text(
-                text = "Theme",
+                text = "Settings",
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center
@@ -77,24 +93,138 @@ fun SettingsScreen(
 
         Spacer(modifier = Modifier.height(2.dp))
 
+
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
+            Text(
+                text = "Theme",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+
             Spacer(modifier = Modifier.height(8.dp))
-            LazyColumn {
-                itemsIndexed(themesList) { index, theme ->
-                    ThemeOption(
-                        title = theme.title,
-                        description = theme.description,
-                        index = index,
-                        isSelected = (selectedTheme?.selectedTheme ?: Theme.SYSTEM.id) == index,
-                        onOptionSelected = {
-                            coroutineScope.launch {
-                                ThemeManager.updateTheme(index)
-                            }
+
+            themesList.forEachIndexed { index, themeData ->
+                ThemeOption(
+                    title = themeData.title,
+                    description = themeData.description,
+                    index = index,
+                    isSelected = (selectedTheme?.selectedTheme ?: Theme.SYSTEM.id) == index,
+                    onOptionSelected = {
+                        coroutineScope.launch {
+                            ThemeManager.updateTheme(index)
                         }
-                    )
+                    }
+                )
+            }
+        }
+
+        // Node Configuration Section
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Node Configuration",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = nodeUrl,
+                onValueChange = { nodeUrl = it },
+                label = { Text("Node URL") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    if (nodeUrl.isNotEmpty()) {
+                        IconButton(onClick = { nodeUrl = "" }) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = "Clear text"
+                            )
+                        }
+                    }
                 }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Username") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    if (username.isNotEmpty()) {
+                        IconButton(onClick = { username = "" }) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = "Clear text"
+                            )
+                        }
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    if (password.isNotEmpty()) {
+                        IconButton(onClick = { password = "" }) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = "Clear text"
+                            )
+                        }
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = port,
+                onValueChange = { port = it },
+                label = { Text("Port") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    if (port.isNotEmpty()) {
+                        IconButton(onClick = { port = "" }) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = "Clear text"
+                            )
+                        }
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                enabled = nodeUrl.isNotEmpty() && username.isNotEmpty()
+                    && password.isNotEmpty() && port.isNotEmpty(),
+                shape = RoundedCornerShape(8.dp),
+                onClick = {
+                    coroutineScope.launch {
+                        // Save the node configuration here
+                        // For example, you could update the store or trigger an event to save the data
+                    }
+                },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text("Save")
             }
         }
     }
@@ -131,6 +261,13 @@ fun ThemeOption(
                 }
             }
         }
+
+        HorizontalDivider(
+            modifier = Modifier
+                .height(1.dp)
+                .padding(top = 10.dp),
+            color = Color.LightGray
+        )
     }
 }
 
