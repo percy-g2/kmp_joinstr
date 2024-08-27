85bbdf97b8682097b954156deefe794cca0ba143
percy-g2 <gahlotpercy@gmail.com>
2024-08-27 23:48:34 +0530

    pre-release v0.0.4
    Signed-off-by: percy-g2 <gahlotpercy@gmail.com>

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -164,8 +164,8 @@ android {
         applicationId = "invincible.privacy.joinstr"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
     }
     packaging {
         resources {
@@ -174,7 +174,9 @@ android {
     }
     buildTypes {
         debug {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         getByName("release") {
