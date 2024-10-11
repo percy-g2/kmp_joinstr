c7382c1e2633208afc07020f4ae161d3f4b9c0ef
percy-g2 <gahlotpercy@gmail.com>
2024-10-11 22:29:42 +0530

    Update NostrEventsTest and adjust Android SDK versions
    - Refactor NostrEventsTest to use predefined test keys
    - Add test for encrypted Nostr event creation and sending
    - Update Android compileSdk and targetSdk versions to 34

--- composeApp/src/commonTest/kotlin/invincible.privacy.joinstr/NostrEventsTest.kt
+++ composeApp/src/commonTest/kotlin/invincible.privacy.joinstr/NostrEventsTest.kt
@@ -1,12 +1,12 @@
 package invincible.privacy.joinstr
 
+import invincible.privacy.joinstr.ktx.hexToByteArray
+import invincible.privacy.joinstr.ktx.toHexString
 import invincible.privacy.joinstr.network.NostrClient
 import invincible.privacy.joinstr.utils.Event
 import invincible.privacy.joinstr.utils.NostrCryptoUtils.createEvent
 import invincible.privacy.joinstr.utils.NostrCryptoUtils.decrypt
 import invincible.privacy.joinstr.utils.NostrCryptoUtils.encrypt
-import invincible.privacy.joinstr.utils.NostrCryptoUtils.generatePrivateKey
-import invincible.privacy.joinstr.utils.NostrCryptoUtils.getPublicKey
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -15,10 +15,14 @@ import kotlin.test.assertNull
 
 class NostrEventsTest {
 
+    // TEST KEY: DO NOT USE THIS
+    private val privateKey = "5be151f3b93b0ddacbdb634c0c1828e3b243073103418541b2c096b108d876f8".hexToByteArray()
+
+    // TEST KEY: DO NOT USE THIS
+    private val publicKey = "9d7dfdc4e5fff62bfd4f94a5c8f586021caa0fc503ca88d4457233b1a17ec11e".hexToByteArray()
+
     @Test
     fun testSuccessfulEncryptionAndDecryptionWithValidSharedSecret() = runTest {
-        val privateKey = generatePrivateKey()
-        val publicKey = getPublicKey(privateKey)
         val sharedSecret = getSharedSecret(privateKey, publicKey)
         // Given
         val message = "This is a secret message"
@@ -35,15 +39,49 @@ class NostrEventsTest {
     }
 
     @Test
-    fun testNostrEventCreationAndSuccessfulSend() = runTest {
-        val privateKey = generatePrivateKey()
-        val publicKey = getPublicKey(privateKey)
-
+    fun testNostrNoteEventCreationAndSuccessfulSend() = runTest {
         // Given
-        val content = "This is a test Nostr event"
+        val content = "This is a test nostr note event"
 
         // When
-        val nostrEvent = createEvent(content, Event.NOTE, privateKey, publicKey)
+        val nostrEvent = createEvent(
+            content = content,
+            event = Event.NOTE,
+            privateKey = privateKey,
+            publicKey = publicKey
+        )
+
+        assertNotNull(nostrEvent, "Event creation should not be null")
+
+        NostrClient().sendEvent(
+            event = nostrEvent,
+            onError = { error ->
+                // Then
+                assertNull(error, "Error callback should not be triggered on successful send")
+            },
+            onSuccess = { eventId ->
+                // Then
+                assertNotNull(eventId, "Event ID should be returned on successful send")
+            }
+        )
+    }
+
+    @Test
+    fun testNostrEncryptedEventCreationAndSuccessfulSend() = runTest {
+        // Given
+        val content = "This is a test nostr encrypted DM event"
+
+        val sharedSecret = getSharedSecret(privateKey, publicKey)
+        val encryptedMessage = encrypt(content, sharedSecret)
+
+        // When
+        val nostrEvent = createEvent(
+            content = encryptedMessage,
+            event = Event.ENCRYPTED_DIRECT_MESSAGE,
+            privateKey = privateKey,
+            publicKey = publicKey,
+            tagPubKey = publicKey.toHexString()
+        )
         assertNotNull(nostrEvent, "Event creation should not be null")
 
         NostrClient().sendEvent(
